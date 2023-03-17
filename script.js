fdd5f321a6aad1906f205502f334f3ebdada2387
John <johnflanagan827@gmail.com>
2023-03-17 01:40:02 -0400

    bug fixes

--- script.js
+++ script.js
@@ -24,208 +24,212 @@ let darkMode = false;
 
 // performs calculator operation
 function calculate(leftOperand, rightOperand, currOperation) {
-  if (isNaN(leftOperand)) {
-    return "Error";
-  }
-
-  let result;
-  if (currOperation === "/") {
-    if (output.textContent === "0") {
-      result = "Error";
-    } else {
-      result = leftOperand / rightOperand;
+    if (isNaN(leftOperand)) {
+        return "Error";
     }
-  } else if (currOperation === "*") {
-    result = leftOperand * rightOperand;
-  } else if (currOperation === "-") {
-    result = leftOperand - rightOperand;
-  } else if (currOperation === "+") {
-    result = leftOperand + rightOperand;
-  }
-  return formatResults(result);
+
+    let result;
+    if (currOperation === "/") {
+        if (output.textContent === "0") {
+            result = "Error";
+        } else {
+            result = leftOperand / rightOperand;
+        }
+    } else if (currOperation === "*") {
+        result = leftOperand * rightOperand;
+    } else if (currOperation === "-") {
+        result = leftOperand - rightOperand;
+    } else if (currOperation === "+") {
+        result = leftOperand + rightOperand;
+    }
+    return formatResults(result);
 }
 
 // formats result properly for display
 function formatResults(result) {
-  if (result === Infinity) {
-    result = "Error";
-  }
-  if (result.toString().length > 10) {
-    result = result.toPrecision(10);
-    if (result.includes("e")) {
-      let e = result.substring(result.indexOf("e"));
-      result = result.substring(0, 10 - e.length) + e;
-    } else {
-      result = result.substring(0, 10);
+    if (result === Infinity) {
+        result = "Error";
     }
-  }
-  return result;
+    if (result.toString().length > 10) {
+        if (result.toString().includes('-')) {
+            result = result.toPrecision(9);
+        } else {
+            result = result.toPrecision(10);
+        }
+        if (result.includes("e")) {
+            let e = result.substring(result.indexOf("e"));
+            result = result.substring(0, 10 - e.length) + e;
+        } else {
+            result = result.substring(0, 10);
+        }
+    }
+    return result;
 }
 
 // performs operation of specified operator
 function performOperation(operator) {
-  if (chained) {
-    rightOperand = output.textContent;
-    output.textContent = calculate(
-      parseFloat(leftOperand),
-      parseFloat(output.textContent),
-      currOperation
-    );
-    leftOperand = output.textContent;
-  } else {
-    leftOperand = output.textContent;
-  }
-  chained = true;
-  enterStatus = false;
-  clearDisplay = true;
-  currOperation = operator;
+    if (chained) {
+        rightOperand = output.textContent;
+        output.textContent = calculate(
+            parseFloat(leftOperand),
+            parseFloat(output.textContent),
+            currOperation
+        );
+        leftOperand = output.textContent;
+    } else {
+        leftOperand = output.textContent;
+    }
+    chained = true;
+    enterStatus = false;
+    clearDisplay = true;
+    currOperation = operator;
 }
 
 // performs equal operation
 function performEqual() {
-  if (!enterStatus) {
-    rightOperand = output.textContent;
-  }
-  output.textContent = calculate(
-    parseFloat(leftOperand),
-    parseFloat(rightOperand),
-    currOperation
-  );
-  leftOperand = output.textContent;
-  enterStatus = true;
-  chained = false;
+    if (!enterStatus) {
+        rightOperand = output.textContent;
+    }
+    output.textContent = calculate(
+        parseFloat(leftOperand),
+        parseFloat(rightOperand),
+        currOperation
+    );
+    leftOperand = output.textContent;
+    enterStatus = true;
+    chained = false;
 }
 
 // changes calculator theme
 function changeTheme(
-  htmlBackground,
-  calculatorBackground,
-  numberBackground,
-  operationBackground,
-  operationOtherBackground,
-  dayNightColor
+    htmlBackground,
+    calculatorBackground,
+    numberBackground,
+    operationBackground,
+    operationOtherBackground,
+    dayNightColor
 ) {
-  document.getElementsByTagName("html")[0].style.backgroundColor =
-    htmlBackground;
-  document.querySelectorAll(".container")[0].style.backgroundColor =
-    calculatorBackground;
-  document.querySelectorAll(".btn-row").forEach((e) => {
-    e.style.backgroundColor = calculatorBackground;
-  });
-  document.getElementById("output").style.backgroundColor =
-    calculatorBackground;
-  document.querySelectorAll(".number").forEach((e) => {
-    e.style.backgroundColor = numberBackground;
-  });
-  document.querySelectorAll(".operation").forEach((e) => {
-    e.style.backgroundColor = operationBackground;
-  });
-  document.querySelectorAll(".operation-other").forEach((e) => {
-    e.style.backgroundColor = operationOtherBackground;
-  });
-  document.querySelectorAll(".day-night").forEach((e) => {
-    e.style.color = dayNightColor;
-  });
+    document.getElementsByTagName("html")[0].style.backgroundColor =
+        htmlBackground;
+    document.querySelectorAll(".container")[0].style.backgroundColor =
+        calculatorBackground;
+    document.querySelectorAll(".btn-row").forEach((e) => {
+        e.style.backgroundColor = calculatorBackground;
+    });
+    document.getElementById("output").style.backgroundColor =
+        calculatorBackground;
+    document.querySelectorAll(".number").forEach((e) => {
+        e.style.backgroundColor = numberBackground;
+    });
+    document.querySelectorAll(".operation").forEach((e) => {
+        e.style.backgroundColor = operationBackground;
+    });
+    document.querySelectorAll(".operation-other").forEach((e) => {
+        e.style.backgroundColor = operationOtherBackground;
+    });
+    document.querySelectorAll(".day-night").forEach((e) => {
+        e.style.color = dayNightColor;
+    });
 }
 
 // EVENT LISTENERS
 numbers.forEach((e) => {
-  e.addEventListener("click", () => {
-    if (
-      output.textContent === "0" ||
-      output.textContent === "Error" ||
-      clearDisplay
-    ) {
-      enterStatus = false;
-      output.textContent = e.value;
-      clearDisplay = false;
-    } else if (output.textContent.length >= 9) {
-      return;
-    } else {
-      if (!(e.value === "." && output.textContent.includes("."))) {
-        output.textContent = output.textContent + e.value;
-      }
-    }
-  });
-  window.addEventListener("keypress", (event) => {
-    if (event.key == e.value) {
-      e.click();
-    }
-  });
+    e.addEventListener("click", () => {
+        if (
+            output.textContent === "0" ||
+            output.textContent === "Error" ||
+            clearDisplay
+        ) {
+            enterStatus = false;
+            output.textContent = e.value;
+            clearDisplay = false;
+        } else if (output.textContent.length >= 9) {
+            return;
+        } else {
+            if (!(e.value === "." && output.textContent.includes("."))) {
+                output.textContent = output.textContent + e.value;
+            }
+        }
+    });
+    window.addEventListener("keypress", (event) => {
+        if (event.key == e.value) {
+            e.click();
+        }
+    });
 });
 
 clear.addEventListener("click", () => {
-  enterStatus = false;
-  chained = false;
-  output.textContent = "0";
-  leftOperand = 0;
+    enterStatus = false;
+    chained = false;
+    output.textContent = "0";
+    leftOperand = 0;
 });
 
 divide.addEventListener("click", () => {
-  performOperation("/");
+    performOperation("/");
 });
 
 multiply.addEventListener("click", () => {
-  performOperation("*");
+    performOperation("*");
 });
 
 subtract.addEventListener("click", () => {
-  performOperation("-");
+    performOperation("-");
 });
 
 add.addEventListener("click", () => {
-  performOperation("+");
+    performOperation("+");
 });
 
 equals.addEventListener("click", () => {
-  performEqual();
+    performEqual();
 });
 
 percent.addEventListener("click", () => {
-  if (output.textContent !== "0") {
-    output.textContent = calculate(parseFloat(output.textContent), 100, "/");
-  }
+    if (output.textContent !== "0") {
+        output.textContent = calculate(parseFloat(output.textContent), 100, "/");
+    }
 });
 
 plusMinus.addEventListener("click", () => {
-  output.textContent = calculate(parseFloat(output.textContent), -1, "*");
+    output.textContent = calculate(parseFloat(output.textContent), -1, "*");
 });
 
 window.addEventListener("keydown", (e) => {
-  if (e.key === "/") {
-    performOperation("/");
-  } else if (e.key === "*") {
-    performOperation("*");
-  } else if (e.key === "-") {
-    performOperation("-");
-  } else if (e.key === "+") {
-    performOperation("+");
-  } else if (e.key === "Enter") {
-    performEqual();
-  } else if (e.key === "%") {
-    if (output.textContent !== "0") {
-      output.textContent = calculate(parseFloat(output.textContent), 100, "/");
+    if (e.key === "/") {
+        performOperation("/");
+    } else if (e.key === "*") {
+        performOperation("*");
+    } else if (e.key === "-") {
+        performOperation("-");
+    } else if (e.key === "+") {
+        performOperation("+");
+    } else if (e.key === "Enter") {
+        performEqual();
+    } else if (e.key === "%") {
+        if (output.textContent !== "0") {
+            output.textContent = calculate(parseFloat(output.textContent), 100, "/");
+        }
+    } else if (e.key === "Backspace") {
+        if (output.textContent.length === 1) {
+            output.textContent = "0";
+        } else if (output.textContent !== "Error") {
+            output.textContent = output.textContent.substring(
+                0,
+                output.textContent.length - 1
+            );
+        }
     }
-  } else if (e.key === "Backspace") {
-    if (output.textContent.length === 1) {
-      output.textContent = "0";
-    } else if (output.textContent !== "Error") {
-      output.textContent = output.textContent.substring(
-        0,
-        output.textContent.length - 1
-      );
-    }
-  }
 });
 
 slider.addEventListener("click", () => {
-  if (darkMode) {
-    changeTheme("#f5dfbb", "#0077b6", "#00b4d8", "#02c39a", "#caf0f8", "black");
-    darkMode = false;
-  } else {
-    changeTheme("#222222", "black", "#505050", "#ff9500", "#d4d4d2", "white");
-    darkMode = true;
-  }
+    if (darkMode) {
+        changeTheme("#f5dfbb", "#0077b6", "#00b4d8", "#02c39a", "#caf0f8", "black");
+        darkMode = false;
+    } else {
+        changeTheme("#222222", "black", "#505050", "#ff9500", "#d4d4d2", "white");
+        darkMode = true;
+    }
 });
 
 changeTheme("#f5dfbb", "#0077b6", "#00b4d8", "#02c39a", "#caf0f8", "black");
